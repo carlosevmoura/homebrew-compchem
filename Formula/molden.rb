6f16f786fa2e179c8c16d01a7ae6a6d582e32557
Carlos de Moura <carlosevmoura@gmail.com>
2020-03-27 16:09:19 +0100

    Added molden.rb

---
+++ Formula/molden.rb
@@ -0,0 +1,29 @@
+class Molden < Formula
+  desc "Pre- and post-processing of molecular and electronic structure"
+  homepage "http://www.cmbi.ru.nl/molden/"
+  url "ftp://ftp.cmbi.ru.nl/pub/molgraph/molden/molden6.2.tar.gz"
+
+  revision 1
+  # tag "chemistry"
+  # doi "10.1023/A:1008193805436"
+
+  depends_on :x11
+  depends_on "gcc" if OS.mac? # for gfortran
+
+  def install
+    system "make"
+    bin.install "molden", "gmolden"
+  end
+
+  def caveats; <<~EOS
+    Two versions of Molden were installed:
+      - gmolden is the full OpenGL version
+      - molden is the Xwindows version
+    EOS
+  end
+
+  test do
+    # molden is an interactive program, there is not much we can test here
+    assert_match "Molden#{version}", shell_output("#{bin}/gmolden -h")
+  end
+end
