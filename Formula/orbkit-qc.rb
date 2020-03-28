bcd00114575070c1d9b44b39bb414c6372babe2d
Carlos de Moura <carlosevmoura@gmail.com>
2020-03-28 14:30:05 +0100

    Update orbkit-qc.rb

--- Formula/orbkit-qc.rb
+++ Formula/orbkit-qc.rb
@@ -5,11 +5,13 @@ class OrbkitQc < Formula
     version "1.0"
     sha256 "85a12305ec4f523dbedcce973f3c004491303cb68fd76f3bf3bafc506df3142e"
 
+    setup_url "https://raw.githubusercontent.com/carlosevmoura/homebrew-compchem/develop/extra_files/orbkit-qc-setup.py"
     depends_on "python3"
+    depends_on "wget"
 
     def install
         system "pip3", "install", "cython", "numpy", "scipy", "h5py", "mayavi"
-        system "wget", "https://raw.githubusercontent.com/carlosevmoura/homebrew-compchem/develop/extra_files/orbkit-qc-setup.py"
+        system "wget", "--no-check-certificate", "--content-disposition", setup_url
         system "python3", "orbkit-qc-setup.py", "build_ext", "--inplace", "clean"
     end
 
