4614960d35134aea1d52786200c25af383b56850
Carlos de Moura <carlosevmoura@gmail.com>
2020-03-29 17:17:41 +0200

    Update avogadro-graph.rb

--- Formula/avogadro-graph.rb
+++ Formula/avogadro-graph.rb
@@ -1,11 +1,11 @@
-cask 'avogadro' do
-    version '1.2.0'
-    sha256 '8a9567a2f3ebf162eab8e375073ea84cd28483f004c7cd3cae33e21864615cc7'
+class MoldenGraph < Formula
+  version '1.2.0'
+  sha256 '8a9567a2f3ebf162eab8e375073ea84cd28483f004c7cd3cae33e21864615cc7'
 
-    # sourceforge.net/avogadro was verified as official when first introduced to the cask
-    url "https://downloads.sourceforge.net/avogadro/Avogadro-#{version}.dmg"
-    name 'Avogadro'
-    homepage 'https://avogadro.cc/wiki/Main_Page'
+  # sourceforge.net/avogadro was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/avogadro/Avogadro-#{version}.dmg"
+  name 'Avogadro'
+  homepage 'https://avogadro.cc/wiki/Main_Page'
 
-    app 'Avogadro.app'
-  end
+  app 'Avogadro.app'
+end
