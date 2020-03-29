8d80575acc554aae76cdb6c55ecfd98d93b06c8b
Carlos de Moura <carlosevmoura@gmail.com>
2020-03-29 18:19:40 +0200

    Update avogadro-graph.rb

--- Formula/avogadro-graph.rb
+++ Formula/avogadro-graph.rb
@@ -1,13 +1,13 @@
-class AvogadroGraph < Formula
-    version '1.2.0'
-    sha256 '8a9567a2f3ebf162eab8e375073ea84cd28483f004c7cd3cae33e21864615cc7'
-
-  # sourceforge.net/avogadro was verified as official when first introduced to the cask
-    url "https://downloads.sourceforge.net/avogadro/Avogadro-1.2.0.dmg"
-    homepage 'https://avogadro.cc/wiki/Main_Page'
-
-    def install
-        system "mv", "-fr", 'Avogadro.app', "/Applications/"
-    end
-
-end
+cask 'avogadro-graph' do
+        version '1.2.0'
+        sha256 '8a9567a2f3ebf162eab8e375073ea84cd28483f004c7cd3cae33e21864615cc7'
+      
+        # sourceforge.net/avogadro was verified as official when first introduced to the cask
+        url "https://downloads.sourceforge.net/avogadro/Avogadro-#{version}.dmg"
+        appcast 'https://sourceforge.net/projects/avogadro/rss',
+                checkpoint: 'b4c194124e797b2c1962828bbbb16c54c624c9f38191bebcb6770259739208df'
+        name 'Avogadro'
+        homepage 'https://avogadro.cc/wiki/Main_Page'
+      
+        app 'Avogadro.app'
+      end
