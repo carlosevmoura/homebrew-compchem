3def2d26cdfcb0f6a19b76b0dbd7c515372b155b
Carlos de Moura <carlosevmoura@gmail.com>
2021-01-13 13:38:04 +0100

    Update molden-graph.rb

--- Formula/molden-graph.rb
+++ Formula/molden-graph.rb
@@ -1,9 +1,9 @@
 class MoldenGraph < Formula
   desc "Pre- and post-processing of molecular and electronic structure"
   homepage "http://www.cmbi.umcn.nl/molden/"
-  url "ftp://ftp.cmbi.umcn.nl/pub/molgraph/molden/molden6.6.tar.gz"
-  version "6.6"
-  sha256 "2a2a7a116a336b607b50e8135bc2cca764c50e4a6896013ee3c6f582b741ee72"
+  url "ftp://ftp.cmbi.umcn.nl/pub/molgraph/molden/molden6.2.tar.gz"
+  version "6.2"
+  sha256 "b6e074dfae52b0b8ade2c44e93f9646dd3bfc2d5a064b5cf892efc5bf6b9a506"
 
   revision 1
   # tag "chemistry"
